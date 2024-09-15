08029f158126ea13d24dd9c7e0c4f4168311ef67
MrbasXTH <thitiwat.srasom@outlook.com>
2024-09-15 20:23:07 +0700

    Revert "fix code"
    This reverts commit dc0a0a68601537bfec2114f5116c7d7e05ccd42d.

--- 02-Venue-Booking-System/script.js
+++ 02-Venue-Booking-System/script.js
@@ -20,42 +20,47 @@ document.getElementById('myForm').addEventListener('submit', function(e) {
 
 /* ฟังก์ชั่นตรวจสอบห้องว่าง */
 function checkAvailability(roomId, starttime, endtime) {
+    /* console.log(roomId, starttime, endtime); */
 
-    const listData = dataJson.filter((item)=>{
-      return (item.roomId == roomId)
-    });
+    const dataFilter = dataJson.filter((item) => {
 
-    if (listData.length == 0) {
-      alert(`ไม่มีห้อง ${roomId} ในระบบ`);
-      return
-    }
-
-
-      const dataFilter = dataJson.filter((listData) => {
       /* แปลงข้อมูล Json ให้เป็นรูปแบบวันที่ */
-      const jsonStartTime = new Date(listData.startTime)
-      const jsonEndTime = new Date(listData.endTime);
-
+      const jsonStartTime = convertDateTime(item.startTime);
+      const jsonEndTime = convertDateTime(item.endTime);
       /* แปลงข้อมูลจาก Input ให้เป็นรูปแบบวันที่ */  
       const reqStartTime = new Date(starttime);
       const reqEndTime = new Date(endtime);
-                          // วันที่14 เวลา19.00 >= วันที่15 เวลา11.00 ( false) ||   วันที่15 เวลา22.00  <=    วันที่14 เวลา12.00 (false)
-      const isRoomAvailable =  !(reqStartTime >= jsonEndTime || reqEndTime <= jsonStartTime)
 
-       // ตรวจสอบห้องที่ตรงกับ roomId แสดงรายการที่อยู่ใน range เป็น true  ออกมา
-      return (listData.roomId == roomId && isRoomAvailable);
+      return (item.roomId == roomId && !(reqStartTime >= jsonEndTime || reqEndTime <= jsonStartTime));
     });
 
-    
     if (dataFilter.length > 0) {
-      console.log("dataFilter",dataFilter);
       alert(`ห้อง ${roomId} ไม่ว่างในวันที่ดังกล่าว !!`);
     } else {
       alert(`ห้อง ${roomId} ว่าง และพร้อมสำหรับการจอง`);
     }
 }
 
+/* ฟังก์ชั่นแปลง string เป็นรูปแบบวันที่ */
+function convertDateTime(input) {
+    // แปลงสตริงวันเวลาให้เป็นวัตถุ Date
+    const [datePart, timePart] = input.split(" ");
+    /* console.log(datePart); */
 
+    const [year, month, day] = datePart.split("-").map(Number);
+    const [hours, minutes, seconds] = timePart.split(":").map(Number);
+
+    const date = new Date(year, month - 1, day, hours, minutes, seconds);
+
+    // สร้างวันที่ใหม่ในรูปแบบ ISO 8601
+    const formattedDate = `${date.getFullYear()}-${String(
+      date.getMonth() + 1
+    ).padStart(2, "0")}-${String(date.getDate()).padStart(2, "0")}T${String(
+      date.getHours()
+    ).padStart(2, "0")}:${String(date.getMinutes()).padStart(2, "0")}`;
+
+    return new Date(formattedDate);
+}
 
   
   
